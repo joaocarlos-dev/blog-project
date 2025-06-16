dedf75e27311d6363623809e8081562eea7c5ecd
ja1zinh0 <joao2011bha@hotmail.com>
2025-06-16 14:38:35 -0300

    added postDate component

---
+++ src/components/PostDate/index.tsx
@@ -0,0 +1,17 @@
+import { formatRelativeDate, formatDatetime } from "@/utils/format-datetime";
+
+type PostDateProps = {
+  dateTime: string;
+};
+
+export function PostDate({ dateTime }: PostDateProps) {
+  return (
+    <time
+      className="text-slate-600 text-sm/tight"
+      dateTime={dateTime}
+      title={formatRelativeDate(dateTime)}
+    >
+      {formatDatetime(dateTime)}
+    </time>
+  );
+}
