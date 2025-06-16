9a2f9cb9c26b7bd2c814a863571d933e5dd65cdf
ja1zinh0 <joao2011bha@hotmail.com>
2025-06-16 08:24:23 -0300

    created post summary component

---
+++ src/components/PostSummary/index.tsx
@@ -0,0 +1,36 @@
+import { formatRelativeDate, formatDatetime } from "@/utils/format-datetime";
+import { PostHeading } from "../PostHeading";
+
+type PostSummaryProps = {
+  postHeading: "h1" | "h2";
+  postLink: string;
+  createdAt: string;
+  title: string;
+  excerpt: string;
+};
+
+export function PostSummary({
+  postHeading,
+  postLink,
+  createdAt,
+  title,
+  excerpt,
+}: PostSummaryProps) {
+  return (
+    <div className="flex flex-col sm:justify-center">
+      <time
+        className="text-slate-600 block text-sm/tight"
+        dateTime={createdAt}
+        title={formatRelativeDate(createdAt)}
+      >
+        {formatDatetime(createdAt)}
+      </time>
+
+      <PostHeading as={postHeading} url={postLink}>
+        {title}
+      </PostHeading>
+
+      <p>{excerpt}</p>
+    </div>
+  );
+}
