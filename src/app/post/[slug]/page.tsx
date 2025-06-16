60488feb5f1ee23b201f342915a0e71de6d21f5a
ja1zinh0 <joao2011bha@hotmail.com>
2025-06-16 10:32:43 -0300

    added generateMetadata to create dynamic title

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,17 +1,28 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostBySlugCached(slug);
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
+
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
-
   const post = await findPostBySlugCached(slug);
 
   return (
     <div>
-      <p>{post.title}</p>
+      <p>{post.content}</p>
     </div>
   );
 }
