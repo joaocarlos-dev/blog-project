a8f7ca86dc813447a4b8700acf54754a46a15c2c
ja1zinh0 <joao2011bha@hotmail.com>
2025-06-23 09:36:48 -0300

    changed delete dialog to a component

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -30,52 +30,6 @@ export default async function AdminPostsListAdmin() {
           </div>
         );
       })}
-
-      <div
-        className={clsx(
-          "fixed z-50 inset-0 bg-black/50 backdrop-blur-xs",
-          "flex items-center justify-center"
-        )}
-      >
-        <div
-          className={clsx(
-            "bg-slate-100 p-6 rounded-lg max-w-2xl mx-6",
-            "flex flex-col gap-6",
-            "shadow-lg shadow-black/30 text-center"
-          )}
-        >
-          <h3 className="text-xl font-bold">Título do DIALOG</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            ultricies facilisis dapibus. Ut interdum tincidunt maximus.
-            Pellentesque in vulputate neque, in lacinia mi. Fusce quis elit quis
-            lorem faucibus molestie nec et massa. Cras varius, quam quis
-            pellentesque euismod, turpis mauris posuere tortor, eu ullamcorper
-            enim sapien nec tortor.
-          </p>
-          <div className="flex items-center justify-around">
-            <button
-              className={clsx(
-                "bg-slate-300 text-slate-950 hover:bg-slate-400",
-                "flex items-center",
-                "py-2 px-4 rounded-lg cursor-pointer"
-              )}
-              autoFocus
-            >
-              Cancelar
-            </button>
-            <button
-              className={clsx(
-                "bg-blue-500 text-blue-50 hover:bg-blue-600",
-                "flex items-center",
-                "py-2 px-4 rounded-lg cursor-pointer"
-              )}
-            >
-              Ok
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
