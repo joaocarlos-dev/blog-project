ae93059ec3a81b43732a22de1a060005b2664d45
ja1zinh0 <joao2011bha@hotmail.com>
2025-06-23 17:38:30 -0300

    created button component

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,23 @@
+import { Button } from "@/components/Button";
+import { BugIcon } from "lucide-react";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
-  return <div className="py-16 text-6xl">Admin post new page</div>;
+  return (
+    <div className="py-16 flex gap-4 flax-wrap items-center">
+      <Button variant={"default"} size="sm">
+        <BugIcon />
+        Confirma{" "}
+      </Button>
+      <Button variant={"ghost"} size="md">
+        <BugIcon />
+        Confirma
+      </Button>
+      <Button variant={"danger"} size="lg">
+        <BugIcon />
+        Confirma
+      </Button>
+    </div>
+  );
 }
