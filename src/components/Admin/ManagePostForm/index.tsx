362e3fa44167d2190c7236837344552f12b5290f
ja1zinh0 <joao2011bha@hotmail.com>
2025-06-30 09:09:14 -0300

    feat: add MarkdownEditor component to ManagePostForm

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -3,14 +3,25 @@
 import { Button } from "@/components/Button";
 import { InputCheckbox } from "../InputCheckbox";
 import { InputText } from "../InputText";
+import { MarkdownEditor } from "@/app/admin/MarkdownEditor";
+import { useState } from "react";
 
 export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState("");
+
   return (
     <form action="">
       <div className="flex flex-col gap-6">
         <InputText labelText="Nome" placeholder="Digite seu nome" />
         <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
         <InputCheckbox />
+        <MarkdownEditor
+          labelText="Content"
+          disabled={false}
+          textAreaName="content"
+          value={contentValue}
+          setValue={setContentValue}
+        />
         <div className="mt-4">
           <Button type="submit" variant={"default"}>
             Enviar
