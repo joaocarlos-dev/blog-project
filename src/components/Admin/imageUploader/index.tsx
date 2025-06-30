94b9aaa280d39ed8bb78dbfbbe756ced261b595b
ja1zinh0 <joao2011bha@hotmail.com>
2025-06-30 09:37:30 -0300

    feat: add image size validation to ImageUploader component

--- src/components/Admin/imageUploader/index.tsx
+++ src/components/Admin/imageUploader/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Button } from "@/components/Button";
+import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
 import { ImageUpIcon } from "lucide-react";
 import { useRef } from "react";
+import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -13,6 +15,29 @@ export function ImageUploader() {
     fileInputRef.current.click();
   }
 
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+
+      toast.error(`Imagem muito grande. Max: ${readableMaxSize}KB`);
+
+      fileInput.value = "";
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    fileInput.value = "";
+  }
+
   return (
     <div className="flex flex-col gap-3 py-4">
       <Button onClick={handleClick} type="button" className="self-start">
@@ -21,6 +46,7 @@ export function ImageUploader() {
       </Button>
 
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className="hidden"
         name="file"
