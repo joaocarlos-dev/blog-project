0e5e6321dccd5795dfebf1a4e11a3433726278f0
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-01 09:50:23 -0300

    feat: enhance ManagePostForm to accept publicPost prop and display post details

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,4 +1,14 @@
+import { ManagePostForm } from "@/components/Admin/ManagePostForm";
+import { makePublicPost } from "@/dto/post/dto";
+import { findPostByIdAdmin } from "@/lib/post/queries/admin";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Editar post",
+};
 
 type AdminPostIdPageProps = {
   params: Promise<{ id: string }>;
@@ -8,6 +18,16 @@ export default async function AdminPostIdPage({
   params,
 }: AdminPostIdPageProps) {
   const { id } = await params;
+  const post = await findPostByIdAdmin(id).catch();
 
-  return <div className="py-16 text-6xl">Admin post id page {id} </div>;
+  if (!post) notFound();
+
+  const publicPost = makePublicPost(post);
+
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-xl font-bold">Editar post</h1>
+      <ManagePostForm publicPost={publicPost} />
+    </div>
+  );
 }
