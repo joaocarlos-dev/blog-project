9d7b25ea1a0028ca6a2467c1cb551e0d7d5fdd9c
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-01 09:34:13 -0300

    feat: reorganize input fields in ManagePostForm for better structure

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -13,17 +13,68 @@ export function ManagePostForm() {
   return (
     <form action="">
       <div className="flex flex-col gap-6">
-        <ImageUploader />
-        <InputText labelText="Nome" placeholder="Digite seu nome" />
-        <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
-        <InputCheckbox />
+        <InputText
+          labelText="ID"
+          name="id"
+          placeholder="ID gerado automaticamente"
+          type="text"
+          defaultValue={""}
+          readOnly
+        />
+
+        <InputText
+          labelText="Slug"
+          name="slug"
+          placeholder="Slug gerado automaticamente"
+          type="text"
+          defaultValue={""}
+          readOnly
+        />
+
+        <InputText
+          labelText="Autor"
+          name="author"
+          placeholder="Digite o nome do autor"
+          type="text"
+          defaultValue={""}
+        />
+
+        <InputText
+          labelText="Título"
+          name="title"
+          placeholder="Digite o título"
+          type="text"
+          defaultValue={""}
+        />
+
+        <InputText
+          labelText="Excerto"
+          name="excerpt"
+          placeholder="Digite o resumo"
+          type="text"
+          defaultValue={""}
+        />
+
         <MarkdownEditor
-          labelText="Content"
-          disabled={false}
-          textAreaName="content"
+          labelText="Conteúdo"
           value={contentValue}
           setValue={setContentValue}
+          textAreaName="content"
+          disabled={false}
         />
+
+        <ImageUploader />
+
+        <InputText
+          labelText="URL da imagem de capa"
+          name="coverImageUrl"
+          placeholder="Digite a URL da imagem"
+          type="text"
+          defaultValue={""}
+        />
+
+        <InputCheckbox labelText="Publicar?" name="published" type="checkbox" />
+
         <div className="mt-4">
           <Button type="submit" variant={"default"}>
             Enviar
