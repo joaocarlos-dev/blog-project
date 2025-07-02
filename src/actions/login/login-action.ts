406b9bf89640e82a5fed3b57e157ea1770611278
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-02 15:20:58 -0300

    feat: implement login action with form validation and session creation

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { createLoginSession, verifyPassword } from "@/lib/login/manage-login";
 import { AsyncDelay } from "@/utils/async-delay";
+import { redirect } from "next/navigation";
 
 type loginActionState = {
   username: string;
@@ -10,8 +12,35 @@ type loginActionState = {
 export async function loginAction(state: loginActionState, formdata: FormData) {
   await AsyncDelay(2000); // Manter para atrasar brute force
 
-  return {
-    username: "",
-    error: "",
-  };
+  if (!(formdata instanceof FormData)) {
+    return {
+      username: "",
+      error: "Dados inválidos",
+    };
+  }
+
+  const username = formdata.get("username")?.toString().trim() || "";
+  const password = formdata.get("password")?.toString().trim() || "";
+
+  if (!username || !password) {
+    return {
+      username,
+      error: "Digite o usuário e a senha",
+    };
+  }
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || ""
+  );
+
+  if (!(isUsernameValid && isPasswordValid)) {
+    return {
+      username,
+      error: "Usuário ou senha inválidos",
+    };
+  }
+
+  await createLoginSession(username);
+  redirect("/admin/post");
 }
