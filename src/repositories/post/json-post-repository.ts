d67f5722352ec7beb5fea503a42dcde3b9015fd3
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-02 13:52:02 -0300

    feat: refactor uploadImageAction and enhance JsonPostRepository with CRUD operations for posts

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,7 +1,7 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
 import { resolve } from "path";
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { SIMULATE_WAIT_IN_MS } from "@/lib/constants";
 
 const ROOT_DIR = process.cwd();
@@ -14,6 +14,12 @@ const JSON_POSTS_FILE_PATH = resolve(
 );
 
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+
+    await new Promise((resolve) => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
     const parsedJson = JSON.parse(jsonContent);
@@ -21,27 +27,28 @@ export class JsonPostRepository implements PostRepository {
     return posts;
   }
 
-  private async simulateWait() {
-    await new Promise((resolve) => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
-  }
-
-  async findAll(): Promise<PostModel[]> {
-    await this.simulateWait();
-
-    const posts = this.readFromDisk();
-    return posts;
+  private async writeToDisk(posts: PostModel[]): Promise<void> {
+    const jsonToString = JSON.stringify({ posts }, null, 2);
+    await writeFile(JSON_POSTS_FILE_PATH, jsonToString, "utf-8");
   }
 
   async findAllPublic(): Promise<PostModel[]> {
     await this.simulateWait();
 
-    const posts = this.readFromDisk();
-    return (await posts).filter((post) => post.published);
+    const posts = await this.readFromDisk();
+    return posts.filter((post) => post.published);
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
+    const posts = await this.readFromDisk();
+    return posts;
   }
 
   async findById(id: string): Promise<PostModel> {
     const posts = await this.findAllPublic();
-    const post = posts.find((post) => post.id == id);
+    const post = posts.find((post) => post.id === id);
 
     if (!post) throw new Error("Post não encontrado para ID");
 
@@ -50,10 +57,68 @@ export class JsonPostRepository implements PostRepository {
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
     const posts = await this.findAllPublic();
-    const post = posts.find((post) => post.slug == slug);
+    const post = posts.find((post) => post.slug === slug);
 
     if (!post) throw new Error("Post não encontrado para slug");
 
     return post;
+  }
+
+  async create(post: PostModel): Promise<PostModel> {
+    const posts = await this.findAll();
+
+    if (!post.id || !post.slug) {
+      throw new Error("Post sem ID ou Slug");
+    }
+
+    const idOrSlugExist = posts.find(
+      (savedPost) => savedPost.id === post.id || savedPost.slug === post.slug
+    );
+
+    if (idOrSlugExist) {
+      throw new Error("ID ou Slug devem ser únicos");
+    }
+
+    posts.push(post);
+    await this.writeToDisk(posts);
+
+    return post;
+  }
+
+  async delete(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const postIndex = posts.findIndex((p) => p.id === id);
+
+    if (postIndex < 0) {
+      throw new Error("Post não existe");
+    }
+
+    const post = posts[postIndex];
+    posts.splice(postIndex, 1);
+    await this.writeToDisk(posts);
+
+    return post;
+  }
+
+  async update(
+    id: string,
+    newPostData: Omit<PostModel, "id" | "slug" | "createdAt" | "updatedAt">
+  ): Promise<PostModel> {
+    const posts = await this.findAll();
+    const postIndex = posts.findIndex((p) => p.id === id);
+    const savedPost = posts[postIndex];
+
+    if (postIndex < 0) {
+      throw new Error("Post não existe");
+    }
+
+    const newPost = {
+      ...savedPost,
+      ...newPostData,
+      updatedAt: new Date().toISOString(),
+    };
+    posts[postIndex] = newPost;
+    await this.writeToDisk(posts);
+    return newPost;
   }
 }
