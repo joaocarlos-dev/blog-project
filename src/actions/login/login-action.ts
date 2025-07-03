6250a81e4168ff2922f069ec76d8025a6042bb18
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-03 10:49:14 -0300

    feat: implement login restrictions based on ALLOW_LOGIN environment variable

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -10,6 +10,15 @@ type loginActionState = {
 };
 
 export async function loginAction(state: loginActionState, formdata: FormData) {
+  const allowLogin = Boolean(Number(process.env.ALLOW_LOGIN));
+
+  if (!allowLogin) {
+    return {
+      username: "",
+      error: "Login not allowed",
+    };
+  }
+
   await AsyncDelay(2000); // Manter para atrasar brute force
 
   if (!(formdata instanceof FormData)) {
--- src/app/admin/post/layout.tsx
+++ src/app/admin/post/layout.tsx
@@ -1,12 +1,15 @@
 import { MenuAdmin } from "@/components/Admin/MenuAdmin";
+import { RequireLoginSessionOrRedirect } from "@/lib/login/manage-login";
 
 type AdminPostLayoutProps = {
   children: React.ReactNode;
 };
 
-export default function AdminPostLayout({
+export default async function AdminPostLayout({
   children,
 }: Readonly<AdminPostLayoutProps>) {
+  await RequireLoginSessionOrRedirect();
+
   return (
     <>
       <MenuAdmin />
