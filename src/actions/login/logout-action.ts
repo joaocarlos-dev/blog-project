07d25b07807b4f190c4597339621703c69ccb34a
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-03 11:23:02 -0300

    feat: add middleware for JWT authentication and redirection for admin routes

--- src/actions/login/logout-action.ts
+++ src/actions/login/logout-action.ts
@@ -1,9 +1,12 @@
 "use server";
 
 import { deleteLoginSession } from "@/lib/login/manage-login";
+import { AsyncDelay } from "@/utils/async-delay";
 import { redirect } from "next/navigation";
 
 export async function logoutAction() {
+  await AsyncDelay(1000); // Manter para atrasar brute force
+
   await deleteLoginSession();
   redirect("/");
 }
